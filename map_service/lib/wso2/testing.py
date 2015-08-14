86e6b133511bdcf49362de60ea0f4127aded86d8
tmkasun <tmkasun@gmail.com>
2015-08-14 08:50:51 +0530

    Add python testing for webservices

--- map_service/lib/wso2/testing.py
+++ map_service/lib/wso2/testing.py
@@ -1,1 +1,11 @@
 __author__ = 'tmkasun'
+
+from services.eventProcessorAdminService import EventProcessor
+from suds import WebFault
+
+evn = EventProcessor()
+try:
+    a = evn.getActiveExecutionPlanConfiguration('geo_speed_alerts')
+    print(a)
+except WebFault, e:
+    print(e)
