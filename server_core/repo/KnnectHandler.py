5b679ee05b0ee80e4861a4f245ec94fd20e63fe8
Kasun Thennakoon <tmkasun@gmail.com>
2017-01-09 09:59:29 +0530

    Put last know position to seperate collection for use when browser load via REST API

--- server_core/repo/KnnectHandler.py
+++ server_core/repo/KnnectHandler.py
@@ -1,5 +1,4 @@
 import logging
-
 from pymongo.results import InsertOneResult
 from tornado import gen
 from tornado.iostream import StreamClosedError, IOStream
@@ -18,6 +17,7 @@ from tornado.web import RequestHandler
 # https://pypi.python.org/pypi/geojson
 # 161205164435,,GPRMC,164435.9,A,0703.5964,N,07957.6741,E,0.0,220.00,051216,0.0,E,A*3E,F,imei:868443028828427,08,,F:3.76V,0,122,,413,01,2C08,F6FE
 
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -32,6 +32,7 @@ class KnnectHandler(TCPServer):
 
     @gen.coroutine
     def handle_stream(self, stream, address):
+        first = True
         while True:
             try:
                 # interGeoJson = handler_in.process()
@@ -42,25 +43,18 @@ class KnnectHandler(TCPServer):
                     data += b"\n"
                 logger.info("Received bytes: %s", data)
                 self.save(data)
-                data_list = list(csv.reader([data])).pop()
-                start = data_list.index('GPRMC')
-                if start:
-                    gprmc = ",".join(data_list[start:15])
-                    data = pynmea2.parse(gprmc)
-                    g_point = geojson.Point((data.longitude, data.latitude))
-                    imei = data_list[16].split(':')[1]
-                    g_properties = {
-                        'heading': data.true_course,
-                        'speed': data.spd_over_grnd,
-                        'state': "NORMAL"
-                    }
-                    g_feature = geojson.Feature(geometry=g_point, id=imei, properties=g_properties)
-                    # self.ws_handler.connections[3].write_message(g_point)
-                    self.notifyClients(g_feature)
+                g_feature = self._tk103_parser(data)
+                self.notify_clients(g_feature)
+                if first:
+                    g_feature['properties']['state'] = 'ONLINE'
+                    self.update_lk_status(g_feature)
+                    first = False
             except StreamClosedError:
+                # Post connection state
                 logger.warning("Lost client at host %s", address[0])
                 g_feature['properties']['state'] = 'OFFLINE'
-                self.notifyClients(g_feature)
+                self.notify_clients(g_feature)
+                self.update_lk_status(g_feature)
                 break
             except Exception as e:
                 print(e)
@@ -70,5 +64,36 @@ class KnnectHandler(TCPServer):
         result = yield self.db.messages.insert_one({'raw': data})
         logger.info("Saved with id = {}".format(result.inserted_id))
 
-    def notifyClients(self, message):
+    def notify_clients(self, message):
         [client.write_message(message) for client in self.ws_handler.connections]
+
+    @gen.coroutine
+    def update_lk_status(self, data):
+        result = yield self.db.testing.update_one({'i': data.id}, {'$set': {'lk_geo_feature': data, 'lk_state':
+            data['properties']['state']}}, upsert=True)
+        logger.info("Update LK_Status with id = {}".format(result.raw_result))
+
+    # @gen.coroutine
+    # def _read_line(self, stream):
+    #     data = yield stream.read_until(b"\n")
+    #     if not data.endswith(b"\n"):
+    #         data += b"\n"
+    #     logger.info("Received bytes: %s", data)
+    #     return data
+
+    def _tk103_parser(self, data):
+        data_list = list(csv.reader([data.decode("utf-8")])).pop()
+        start = data_list.index('GPRMC')
+        if start:
+            gprmc = ",".join(data_list[start:15])
+            data = pynmea2.parse(gprmc)
+            g_point = geojson.Point((data.longitude, data.latitude))
+            imei = data_list[16].split(':')[1]
+            g_properties = {
+                'heading': data.true_course,
+                'speed': data.spd_over_grnd,
+                'state': "NORMAL"
+            }
+            g_feature = geojson.Feature(geometry=g_point, id=imei, properties=g_properties)
+            # self.ws_handler.connections[3].write_message(g_point)
+        return g_feature
--- server_core/testing/corouting.py
+++ server_core/testing/corouting.py
@@ -1,25 +1,59 @@
-import asyncio
-from collections import Coroutine
-from dis import Bytecode
+# import asyncio
+# from collections import Coroutine
+# from dis import Bytecode
+#
+#
+# @asyncio.coroutine
+# def countdown(name, n):
+#     while n > 0:
+#         print('T-minus', n, '({})'.format(name))
+#         yield from asyncio.sleep(1)
+#         n -= 1
+#
+# a = Bytecode(countdown)
+# for i in a:
+#     print(i)
+# Coroutine
+# assert False
+# loop = asyncio.get_event_loop()
+# tasks = [
+#     asyncio.ensure_future(countdown("A", 5)),
+#     asyncio.ensure_future(countdown("B", 10))
+# ]
+# loop.run_until_complete(asyncio.wait(tasks))
+# print("closing")
+# loop.close()
+
+#
+# class A(object):
+#
+#     def __init__(self):
+#         name = "Kasun"
+#
+#     def meth1(self,arg1):
+#         print("inside meth1")
+#         try:
+#             self.meth2("arg_to meth2")
+#         except Exception as e:
+#             print(e)
+#
+#     def meth2(self,data):
+#         print("inside meth2")
+#         raise Exception("Wollaa custome exception")
+#
+#
+# b = A()
+# b.meth1("asda")
+from tornado.ioloop import IOLoop
+from tornado.gen import coroutine
+from motor.motor_tornado import MotorClient
 
 
-@asyncio.coroutine
-def countdown(name, n):
-    while n > 0:
-        print('T-minus', n, '({})'.format(name))
-        yield from asyncio.sleep(1)
-        n -= 1
+@coroutine
+def do_update(data):
+    db = MotorClient().knnect
+    result = yield db.testing.update_one({'i': data.id}, {'$set': {'lk_geo_feature': data}}, upsert=True)
+    print(result)
 
-a = Bytecode(countdown)
-for i in a:
-    print(i)
-Coroutine
-assert False
-loop = asyncio.get_event_loop()
-tasks = [
-    asyncio.ensure_future(countdown("A", 5)),
-    asyncio.ensure_future(countdown("B", 10))
-]
-loop.run_until_complete(asyncio.wait(tasks))
-print("closing")
-loop.close()
+
+IOLoop.current().run_sync(do_update)
