8eb3fae93ea422a0104d73564b4a568f0f075848
Kasun Thennakoon <tmkasun@gmail.com>
2017-02-22 08:34:14 +0530

    Change localhost url

--- map_system/static/map_system/local/libraries/services.js
+++ map_system/static/map_system/local/libraries/services.js
@@ -16,7 +16,7 @@ class MapService {
      */
     constructor() {
         this.service_endpoint = "/apis/";
-        this.baseURL = 'http://localhost:8000/apis';
+        this.baseURL = 'http://geo.knnect.com/apis';
         this.client = request;
         // this.client.domain = this.baseURL;
     }
