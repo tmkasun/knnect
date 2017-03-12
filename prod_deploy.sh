a4c6a5d9c51e7d733acec8f19de7af5ee3f077a5
Kasun Thennakoon <tmkasun@gmail.com>
2017-03-12 22:05:51 +0530

    Add info messages to deployment script

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -2,10 +2,17 @@
 # TODO: This needs to be replaced with proper puppet script
 branch="tornado-backend"
 
-echo "Deploying for production..."
+echo "######### {INFO} : Deploying for production... #########"
+echo "######### {INFO} : Current GIT status #########"
 git status
+echo "######### {INFO} : Sync via GIT #########"
 git pull origin ${branch}
+echo "######### {INFO} : Webpack operation #########"
 webpack --context ./map_system/static/map_system/local/libraries/  --config ./map_system/static/map_system/local/libraries/webpack.config.js
+echo "######### {INFO} : Django collecting static files #########"
 python3.5 manage.py collectstatic --noinput -i node_modules
+echo "######### {INFO} : Restarting apache server #########"
 sudo systemctl restart apache2.service
-echo "All done"
+echo "######### {INFO} : Check for currently running server cores #########"
+ps aux | grep -v grep | grep -i -e VSZ -e server
+echo "######### {INFO} : All done... #########"
